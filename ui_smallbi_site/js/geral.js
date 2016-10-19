24b611edfb498c3bfe9099c2ffadb4a5e28fa886
sanzio santos <sanztss@gmail.com>
2016-10-19 13:46:17 -0200

    front site finalizado

--- ui_smallbi_site/js/geral.js
+++ ui_smallbi_site/js/geral.js
@@ -2,4 +2,25 @@
 	$(function(){
 		var windowWidth = $(window).width() / 2 + 125;
 		$('h1').width(windowWidth);
+		$(window).resize(function(){
+			var windowWidth = $(window).width() / 2 + 125;
+			$('h1').width(windowWidth);
+		});
+
+		$("#btnModalLogin").click(function(e){
+			e.preventDefault();
+			$("#modalLogin").css({"opacity": 1, "visibility":"visible"});
+		});
+
+		$("#btnModalContato").click(function(e){
+			e.preventDefault();
+			$("#modalContato").css({"opacity": 1, "visibility":"visible"});
+		});
+
+		$(".btnFecharModal").click(function(e){
+			e.preventDefault();
+			console.log('oi');
+			$(".dialog").css({"opacity": 0, "visibility":"hidden"});
+		});
+		
 	});
