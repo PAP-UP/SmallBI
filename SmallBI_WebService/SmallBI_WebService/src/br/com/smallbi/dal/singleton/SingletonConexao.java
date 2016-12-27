fb3532f322621b9d6cfe2a535550fc3408bc2222
ersanbr <ers2003@gmail.com>
2016-12-27 19:07:38 -0200

    Ajustado nome do Database

--- SmallBI_WebService/SmallBI_WebService/src/br/com/smallbi/dal/singleton/SingletonConexao.java
+++ SmallBI_WebService/SmallBI_WebService/src/br/com/smallbi/dal/singleton/SingletonConexao.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class SingletonConexao {
 
-	public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("smallbiDatabase");
+	public static EntityManagerFactory emf = Persistence.createEntityManagerFactory("smallbi");
 	
 	public static EntityManager getInstance(){
 		return emf.createEntityManager();
