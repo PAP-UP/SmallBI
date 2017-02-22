b280d05d4354ef9e6dddac710b5052f192496ee5
Raphael Gumm <raphael-gumm@hotmail.com>
2017-02-22 14:16:58 -0300

    Ajuste no resource de função

--- SmallBI_FrontEnd/app/scripts/funcao/funcao.resource.js
+++ SmallBI_FrontEnd/app/scripts/funcao/funcao.resource.js
@@ -7,14 +7,38 @@
   function funcaoResource(Service) {
 
     var service = {
-      listaFuncao: listaFuncao
+      listaFuncoes: listaFuncoes,
+	  insereFuncao:insereFuncao,
+	  alteraFuncao: alteraFuncao,
+	  getFuncaoById: getFuncaoById,
+	  deleteFuncao: deleteFuncao
     };
 
     return service;
 
-    function listaFuncao() {
+    function listaFuncoes() {
       var url = 'funcao/listar';
       return Service.serviceGet(url);
     }
+	
+	function insereFuncao(dataParam) {
+      var url = 'funcao/adicionar';
+      return Service.servicePost(dataParam, url);
+	}
+
+	function alteraFuncao(dataParam) {
+		  var url = 'funcao/alterar';
+		  return Service.servicePost(dataParam, url);
+	}
+	
+	function getFuncaoById(param) {
+      var url = 'funcao/getById/';
+      return Service.serviceGetById(param, url);
+	}
+	
+	function deleteFuncao(param) {
+      var url = 'funcao/deletar/';
+      return Service.serviceDelete(param, url);
+	}
   }
 })();
