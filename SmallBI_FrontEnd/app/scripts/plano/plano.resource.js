0fcb6c17f9dae435a5022f2ce5875c9b3d980812
Raphael Gumm <raphael-gumm@hotmail.com>
2017-02-22 14:06:54 -0300

    Ajuste no arquivo resource de Ramo Atividade

--- SmallBI_FrontEnd/app/scripts/plano/plano.resource.js
+++ SmallBI_FrontEnd/app/scripts/plano/plano.resource.js
@@ -2,43 +2,43 @@
   'use strict';
 
   angular.module('SmallBIApp')
-    .factory('planoResource', planoResource);
+    .factory('ramoResource', ramoResource);
 
-  function planoResource(Service) {
+  function ramoResource(Service) {
 
     var service = {
-      listaPlanos: listaPlanos,
-      inserePlano: inserePlano,
-      alteraPlano: alteraPlano,
-      getPlanoById: getPlanoById,
-      deletePlano: deletePlano
+      listaRamos: listaRamos,
+	  insereRamo:insereRamo,
+	  alteraRamo: alteraRamo,
+	  getRamoById: getRamoById,
+	  deleteRamo: deleteRamo
     };
 
     return service;
 
-    function listaPlanos() {
-      var url = 'plano/listar';
+    function listaRamos() {
+      var url = 'ramoAtividade/listar';
       return Service.serviceGet(url);
     }
-
-    function inserePlano(dataParam) {
-      var url = 'plano/adicionar';
+	
+	function insereRamo(dataParam) {
+      var url = 'ramoAtividade/adicionar';
       return Service.servicePost(dataParam, url);
-    }
+	}
 
-    function alteraPlano(dataParam) {
-      var url = 'plano/alterar';
-      return Service.servicePost(dataParam, url);
-    }
-
-    function getPlanoById(param) {
-      var url = 'plano/getById/';
+	function alteraRamo(dataParam) {
+		  var url = 'ramoAtividade/alterar';
+		  return Service.servicePost(dataParam, url);
+	}
+	
+	function getRamoById(param) {
+      var url = 'ramoAtividade/getById/';
       return Service.serviceGetById(param, url);
-    }
-
-    function deletePlano(param) {
-      var url = 'plano/deletar/';
+	}
+	
+	function deleteRamo(param) {
+      var url = 'ramoAtividade/deletar/';
       return Service.serviceDelete(param, url);
-    }
+	}
   }
 })();
