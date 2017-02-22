781c59b3f837d32fb1622481144496681bc45187
Raphael Gumm <raphael-gumm@hotmail.com>
2017-02-22 14:13:51 -0300

    criação das controllers de Função

---
+++ SmallBI_FrontEnd/app/scripts/funcao/funcao-listar.controller.js
@@ -0,0 +1,37 @@
+(function () {
+  'use strict';
+
+  angular.module('SmallBIApp')
+    .controller('funcaoListarController', funcaoListarController);
+
+  function funcaoListarController(ramoResource, $state) {
+
+    var vm = this;
+
+    angular.extend(vm, {
+      excluirFuncao: excluirFuncao
+    });
+
+    function listaFuncoes() {
+      funcaoResource.listaFuncoes().then(function (result) {
+        vm.listaFuncoes = result.data;
+      });
+    }
+
+    function excluirFuncao(id) {
+      funcaoResource.deleteFuncao(id).then(
+        function (result) {
+          console.log(result);
+          $state.reload();
+        });
+    }
+
+    function activate() {
+      listaFuncoes();
+
+    }
+
+    activate();
+
+  }
+})();
