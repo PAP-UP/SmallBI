53265bdf39587ea9b7bb0b8bb66eda451a0083dd
Raphael Gumm <raphael-gumm@hotmail.com>
2017-03-29 12:16:47 -0300

    Armazenando informações no cookie

--- SmallBI_FrontEnd2/app/scripts/AuthService.js
+++ SmallBI_FrontEnd2/app/scripts/AuthService.js
@@ -1,32 +1,36 @@
 (function () {
   'use strict';
 
-angular.module('SmallBIApp')
-  .factory('AuthService', AuthService);
+  angular.module('SmallBIApp')
+    .factory('AuthService', AuthService);
 
-function AuthService ($http, $q) {
-  return {
-    // getToken : function () {
-    //   return $localStorage.token;
-    // },
-    // setToken: function (token) {
-    //   $localStorage.token = token;
-    // },
-    signin : function (data) {
-      $http.post('http://backend.smallbi.com.br:18080/SmallBI_WebService/rest/usuario/login', data).then(
-        function (result) {
-
-      }, function () {
-
-        });
-    },
-    signup : function (data) {
-      $http.post('api/signup', data);
-    },
-    logout : function (data) {
-      delete $localStorage.token;
-      $q.when();
-    }
-  };
-}
+  function AuthService($http, $q, $cookies) {
+    return {
+      getToken : function () {
+        return $cookies.token;
+      },
+      // setToken: function (token) {
+      //   $localStorage.token = token;
+      // },
+      signin: function (data) {
+        $http.post('http://backend.smallbi.com.br:18080/SmallBI_WebService/rest/usuario/login', data).then(
+          function (result) {
+            $cookies = result;
+            // $cookies.token = result.token;
+            // $cookies.user_id = result.idUsuario;
+            // $cookies.client_id = result.idCliente;
+            console.log(result);
+          }, function (resolve) {
+            console.log(resolve);
+          });
+      },
+      signup: function (data) {
+        $http.post('api/signup', data);
+      },
+      logout: function (data) {
+        delete $localStorage.token;
+        $q.when();
+      }
+    };
+  }
 })();
