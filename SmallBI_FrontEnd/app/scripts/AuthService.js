ff7a7a03655379c9817a15d575a4413ed9842365
rggumm <raphael-gumm@hotmail.com>
2017-03-29 15:22:21 -0300

    autenticacao e permissao no sistema

--- SmallBI_FrontEnd/app/scripts/AuthService.js
+++ SmallBI_FrontEnd/app/scripts/AuthService.js
@@ -4,17 +4,24 @@
   angular.module('SmallBIApp')
     .factory('AuthService', AuthService);
 
-  function AuthService($http, $q, $cookies) {
+  function AuthService($http, $q, $cookies, $cookieStore) {
     return {
       getToken : function () {
-        // return $q(function (resolve, reject) {
-        //   $http.post('http://backend.smallbi.com.br:18080/SmallBI_WebService/rest/usuario/checarToken', $cookies.token).then(
-        //     function (result) {
-        //       resolve(result);
-        //     }, function (response) {
-        //       reject(response);
-        //     });
-        // });
+        if($cookieStore.get('success')) {
+          var obj = {};
+          obj.token = $cookieStore.get('TOKEN');
+          return $q(function (resolve, reject) {
+            $http.post('http://backend.smallbi.com.br:18080/SmallBI_WebService/rest/usuario/checarToken', obj).then(
+              function (result) {
+                resolve(result);
+              }, function (response) {
+                reject(response);
+              });
+          });
+        }else {
+          return false;
+        }
+
       },
       // setToken: function (token) {
       //   $localStorage.token = token;
@@ -24,7 +31,11 @@
           $http.post('http://backend.smallbi.com.br:18080/SmallBI_WebService/rest/usuario/login', data).then(
             function (result) {
               resolve(result);
-              $cookies = result.data;
+              $cookieStore.put('idUsuario', result.data.idUsuario);
+              $cookieStore.put('idCliente', result.data.idCliente);
+              $cookieStore.put('TOKEN', result.data.token);
+              $cookieStore.put('userName', result.data.nome);
+              $cookieStore.put('success', result.data.success);
             }, function (response) {
               reject(response);
             });
