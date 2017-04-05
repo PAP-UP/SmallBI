31b966093487867e6ba29e4a8524c0216abbda39
Deyneson Borba <deynesonborba@gmail.com>
2017-04-04 22:18:22 -0300

    Ajuste permissionamento

--- SmallBI_WebService/SmallBI_WebService/src/br/com/smallbi/business/UrlBusiness.java
+++ SmallBI_WebService/SmallBI_WebService/src/br/com/smallbi/business/UrlBusiness.java
@@ -36,40 +36,12 @@ public class UrlBusiness implements InterfaceBusiness<Url>{
 		return new UrlDao().getObjById(id);
 	}
 	
-	public Url getUrlByUrl(String urlParam){
+	public boolean hasAccess(String urlParam, Integer idPerfil){
 		List<Url> urls = list();
 		for(Url u : urls){
-			if(u.getUrl().equals(urlParam))
-				return u;
-		}
-		return null;
-	}
-
-	public boolean hasAccess(String urlParam, Integer idPerfil){
-		
-		Url url = getUrlByUrl(urlParam);
-
-		if(urlParam.equals("usuario.listar") || urlParam.equals("usuario.cadastrar") || 
-				urlParam.equals("usuario.editar")){
-			if(idPerfil == 1 || idPerfil == 2)
+			if(u.getUrl().equals(urlParam) && u.getPerfil().getIdPerfil() == idPerfil)
 				return true;
 		}
-
-		if(urlParam.equals("cubo.listar")){
-			if(idPerfil == 1 || idPerfil == 2)
-				return true;
-		}
-
-		if(urlParam.equals("cliente.listar") || urlParam.equals("cliente.cadastrar") ||
-				urlParam.equals("cliente.editar")){
-			if(idPerfil == 1 || idPerfil == 2)
-				return true;
-		}
-		
-		if(url.getPerfil().getIdPerfil() == idPerfil){
-			return true;
-		}
-		
 		return false;
 	}	
 }
