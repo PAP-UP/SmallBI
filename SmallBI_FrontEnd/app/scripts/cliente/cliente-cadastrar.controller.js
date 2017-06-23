9775fbf6f25ece3e2ed8475a8ae763ee518a4e36
rggumm <raphael-gumm@hotmail.com>
2017-06-22 21:45:20 -0300

    cliente cadastrar

--- SmallBI_FrontEnd/app/scripts/cliente/cliente-cadastrar.controller.js
+++ SmallBI_FrontEnd/app/scripts/cliente/cliente-cadastrar.controller.js
@@ -28,7 +28,7 @@
         vm.dadosCliente.idPerfil = 2;
       vm.dadosCliente.idCidade = vm.dadosCliente.Cidade.idCidade;
       delete vm.dadosCliente.Cidade;
-      
+
       clienteResource.insereCliente(vm.dadosCliente).then(
         function (result) {
           $('#modalCadastrar').hide();
